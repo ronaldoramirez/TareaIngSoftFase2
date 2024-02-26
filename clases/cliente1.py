6d2213d04a8580eafe237425c5ea34f6b4cb2308
Brandon474772 <brandontocm2000@gmail.com>
2024-02-25 23:45:49 -0600

    creacion de cliente1

---
+++ clases/cliente1.py
@@ -0,0 +1,17 @@
+import Persona
+class Cliente1():
+    def __init__(self):
+       super()
+       self.Nit 
+
+    def crear_Cliente1(self):
+        print("Se crea una Cliente1")
+
+    def leer_Cliente1(self):
+        print("Se lee el listado de Cliente1")
+
+    def actualizar_Cliente1(self):
+        print("Se actualiza una Cliente1")
+
+    def borra_Cliente1(self):
+        print("Se borra una Cliente1")
